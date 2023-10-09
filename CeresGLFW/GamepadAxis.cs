518328a5ad11da256d4c32fb6714588da91c5a7d
Sam Fuller <sam@samfullerstudios.com>
2023-10-08 20:22:14 -0700

    Add GamepadAxis enum.

---
+++ CeresGLFW/GamepadAxis.cs
@@ -0,0 +1,12 @@
+﻿namespace CeresGLFW
+{
+    public enum GamepadAxis
+    {
+        LEFT_X = 0,
+        LEFT_Y = 1,
+        RIGHT_X = 2,
+        RIGHT_Y = 3,
+        LEFT_TRIGGER = 4,
+        RIGHT_TRIGGER = 5
+    }
+}
